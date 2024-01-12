668ffc74f0fa7be87eb86267ce40b10ae24c8b49
haolin <haolin-yang@foxmail.com>
2024-01-12 10:29:30 +0800

    update path convert to windows

--- PathConvert.py
+++ PathConvert.py
@@ -4,7 +4,12 @@ __author__ = 'IT小叮当'
 __time__ = '2024-01-12 10:14'
 
 
-
+def path2windows(path):
+    '''
+    path: the path of Linux in the form of string
+    '''
+    wpath = path.replace('/', '\\')
+    return wpath
 
 
 
@@ -12,3 +17,5 @@ __time__ = '2024-01-12 10:14'
 
 if __name__ == "__main__":
     print("hello world")
+    p = 'G:\\Clothing-1M\\images/2/11/815467435,1984292211.jpg'
+    print(path2windows(p))
